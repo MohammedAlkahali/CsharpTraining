ce8258f7f0ee2aac2ecdc2895d306cdb76818b2c
MohammedAlkahali <mohammedharib21@gmail.com>
2026-05-12 15:43:08 +0400

    Implement task 1 - System Setup

--- BankingService/Program.cs
+++ BankingService/Program.cs
@@ -1,10 +1,164 @@
-﻿namespace BankingService
+﻿using System.Diagnostics.Metrics;
+using System.Security.Principal;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace BankingService
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Task 1
+
+            int accountNumber = 0;
+
+            string holderName = "";
+
+            double balance = 0.000;
+
+            bool isActive = false;
+
+            char accountType = 'A';
+
+            bool isEmployed = false;
+
+            double MonthlySalary = 0.000;
+
+            int CreditScore = 0;
+
+            int Age = 0;
+
+            double deposit = 0.000;
+
+            double withdrawal = 0.000;
+
+            double annualRate = 0.000;
+
+            double avgBalance = 0.000;
+
+
+            Console.WriteLine("""
+                        --- Account Profile ---
+             1) Account Number (int) current: 0
+             2) Holder Name (string) current: [not set]
+             3) Balance (double) current: 0.000 OMR
+             4) Account Active? (bool) current: false [enter 1=yes / 0=no]
+             5) Account Type (char) current: - [enter S / C / F]
+
+                        --- Customer Profile ---
+             6) Employed? (bool) current: false [enter 1=yes / 0=no]
+             7) Monthly Salary (double) current: 0.000 OMR
+             8) Credit Score (int) current: 0
+             9) Age (int) current: 0
+
+                        --- Transaction Data ---
+             10) Last Deposit Amount (double) current: 0.000 OMR
+             11) Last Withdrawal (double) current: 0.000 OMR
+             12) Annual Interest Rate (double) current: 0.000 OMR  [e.g. 0.035 = 3.5%]
+             13) Avg Monthly Balance (double) current: 0.000 OMR
+
+             0) Setup complete — launch Main Menu
+
+
+             """);
+            
+
+
+            Console.WriteLine("Choose an Option");
+
+
+            int setupMenu = Convert.ToInt32(Console.ReadLine());
+                switch(setupMenu)
+                {
+
+                    case 0:
+                        Console.WriteLine("Setup complete. Launching Main Menu...");
+                        break;
+
+                    case 1:
+                        Console.WriteLine("enter account number: ");
+                        accountNumber = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine($"Account number :  {accountNumber}");
+                        break; 
+
+                    case 2:
+                        Console.WriteLine("enter holder name");
+                        holderName = Console.ReadLine();
+                        Console.WriteLine($"holder name :  {holderName}");
+                        break;
+
+                    case 3:
+                        Console.WriteLine("enter current balance");
+                        balance = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine($"current balance :  {balance}");
+                        break;
+
+                    case 4:
+                        Console.WriteLine("enter 1=active / 0=inactive");
+                        isActive = Convert.ToBoolean(Console.ReadLine());
+                        Console.WriteLine($"account active :  {isActive}");
+                        break;
+
+                    case 5:
+                        Console.WriteLine("enter S / C / F");
+                        accountType = Convert.ToChar (Console.ReadLine());
+                        Console.WriteLine($"account type :  {accountType}");
+                        break;
+
+                    case 6:
+                        Console.WriteLine("enter 1=employed / 0=not");
+                        isEmployed = Convert.ToBoolean(Console.ReadLine());
+                        Console.WriteLine($"Employee status :  {isEmployed}");
+                        break;
+
+                    case 7:
+                        Console.WriteLine("enter monthly salary");
+                        MonthlySalary = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine($"Salary :  {MonthlySalary}");
+                        break;
+
+                    case 8:
+                        Console.WriteLine("enter credit score");
+                        CreditScore = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine($"Credit score :  {CreditScore}");
+                        break;
+
+                    case 9:
+                        Console.WriteLine("enter customer age");
+                        Age = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine($"Age :  {Age}");
+                        break;
+
+                    case 10:
+                        Console.WriteLine("enter last deposit amount");
+                        deposit = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine($"deposit :  {deposit}");
+                        break;
+
+                    case 11:
+                        Console.WriteLine("enter last withdrawal");
+                        withdrawal = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine($"withdrawal :  {withdrawal}");
+                        break;
+
+                    case 12:
+                        Console.WriteLine("enter annual interest rate");
+                        annualRate = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine($"annual rate :  {annualRate}");
+                        break;
+
+                    case 13:
+                        Console.WriteLine("enter avg monthly balance");
+                        avgBalance = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine($"Average :  {avgBalance}");
+                        break;
+
+                    default : Console.WriteLine("Invalid option. Please choose 1–13 or 0 to finish.");
+                    break;
+                }
         }
+
+            //Task 2 
+
     }
 }
